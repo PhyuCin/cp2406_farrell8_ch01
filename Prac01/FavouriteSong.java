ba38f2670ba709b08028378a4caa95183a51d5d9
PhyuCin <ayemyaphyucin@my.jcu.edu.au>
2019-03-21 22:54:17 +0800

    complete FavouriteSong exercise

---
+++ Prac01/FavouriteSong.java
@@ -0,0 +1,5 @@
+public class FavouriteSong {
+    public static void main(String[] args){
+        System.out.println("Even if the sun hides\nYou will shine\nDon’t forget the promise\nThat shines like the stars");
+    }
+}
