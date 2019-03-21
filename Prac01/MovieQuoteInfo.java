3bbcd0760ea15b78e273fad2de778ee1dbf77d5f
PhyuCin <ayemyaphyucin@my.jcu.edu.au>
2019-03-21 22:43:37 +0800

    complete MovieQuoteInfo exercise

---
+++ Prac01/MovieQuoteInfo.java
@@ -0,0 +1,7 @@
+public class MovieQuoteInfo {
+    public static void main(String[] args)
+    {
+        System.out.println("May the Force be with you.");
+        System.out.println("- Han Solo (Star Wars, 1977)");
+    }
+}
