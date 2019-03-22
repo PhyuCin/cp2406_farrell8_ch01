ea4668c5ddbe7d022f2bffec5b91bdcc15429390
PhyuCin <ayemyaphyucin@my.jcu.edu.au>
2019-03-22 10:51:44 +0800

    fix minor things

--- Prac01/CommentsDialog.java
+++ Prac01/CommentsDialog.java
@@ -3,7 +3,7 @@ import javax.swing.*;
 public class CommentsDialog {
     public static void main(String[] args) {
         JOptionPane.showMessageDialog(null, "Program comments are nonexecuting statements you" +
-                "add to a file for the purpose of documentation");
+                " add to a file for the purpose of documentation.");
         //Program comments are nonexecuting statements you add to a file for the purpose of documentation
         /*Program comments are nonexecuting statements you add to a file for the purpose of documentation*/
     }
